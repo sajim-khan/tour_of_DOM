2cc96892c11c7e8fc43a2720d2ac37d793edd4c3
sajim-khan <shossain1427@gmail.com>
2023-02-10 22:30:56 +0600

    added static section

--- js/append.js
+++ js/append.js
@@ -19,12 +19,28 @@ const ul = document.createElement('ul');
 const li1 = document.createElement('li');
 li1.innerText = 'Biriany'
 ul.appendChild(li1)
+
 const li2 = document.createElement('li');
 li2.innerText = 'Kacchi'
-ul.appendChild(li1)
+ul.appendChild(li2)
+
 const li3 = document.createElement('li');
 li3.innerText = 'Beef'
-ul.appendChild(li1)
+ul.appendChild(li3)
 
-section.innerText = 'Added section';
+section.appendChild(ul);
 mainContainer.appendChild(section);
+
+
+//Another way to create a section dynamically
+const sectionDress = document.createElement('section');
+sectionDress.innerHTML = `
+    <h1>My dress section</h1>
+    <ul>
+        <li> T-Shirt </li>
+        <li> Shirt </li>
+        <li> Pant </li>
+        <li> Panjabi </li>
+    </ul>
+`
+mainContainer.appendChild(sectionDress);
