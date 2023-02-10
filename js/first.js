3e0619765cd2e4736d794240914eecd1fc26e224
sajim-khan <shossain1427@gmail.com>
2023-02-10 22:57:02 +0600

    added js style file

--- js/first.js
+++ js/first.js
@@ -1,1 +1,7 @@
 console.log('Added first file')
+
+const allLi = document.getElementsByTagName('li')
+console.log(allLi);
+
+const allTitles = document.getElementsByClassName('section-title')
+console.log(allTitles);
