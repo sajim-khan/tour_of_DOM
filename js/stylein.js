7293fab697a38d6378007405c7e35800b7cb4760
sajim-khan <shossain1427@gmail.com>
2023-02-10 22:48:40 +0600

    added summary file

--- js/stylein.js
+++ js/stylein.js
@@ -4,7 +4,7 @@ for ( const section of sections ) {
     section.style.border = '2px solid'
     section.style.marginBottom = '5px';
     section.style.borderRadius = '15px';
-    section.style.backgroundColor = 'black'
+    section.style.backgroundColor = 'black';
 }
 
 // const placesContainer = document.getElementById('places-container');
