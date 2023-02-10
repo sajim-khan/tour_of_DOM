c7ec167b4dac0864bca3321f94622ed94f396350
sajim-khan <shossain1427@gmail.com>
2023-02-10 23:17:26 +0600

    added section

---
+++ js/third.js
@@ -0,0 +1,12 @@
+const main = document.getElementById('main-container')
+const section = document.createElement('section')
+section.innerHTML = `
+<h1>My dress section</h1>
+<ul>
+    <li> T-Shirt </li>
+    <li> Shirt </li>
+    <li> Pant </li>
+    <li> Panjabi </li>
+</ul>
+`
+main.appendChild(section)
